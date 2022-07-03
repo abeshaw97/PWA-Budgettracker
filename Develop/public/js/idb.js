847b1a8aedb147c473c1e9e6e336725ca5fe503d
abeshaw97 <shawabra@msu.edu>
2022-07-03 15:07:44 -0400

    updated rest of file to keep track of transactions of the store

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -20,3 +20,68 @@ request.onsuccess = function (event) {
     uploadTransactions(db);
   }
 };
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+  };
+  
+  function saveRecord(record) {
+    /* open new db transaction with read and write permissions */
+    const transaction = db.transaction(["new_transaction"], "readwrite");
+  
+    /* access object store for "new_transaction" */
+    const transactionObjectStore = transaction.objectStore("new_transaction");
+  
+    
+    transactionObjectStore.add(record);
+  }
+  
+  /* function uploads data from IndexedDB store to the db */
+  function uploadTransactions() {
+    //* open a new IndexedDB db transction */
+    const transaction = db.transaction(["new_transaction"], "readwrite");
+  
+    /* access object store for "new_transaction" */
+    const transactionObjectStore = transaction.objectStore("new_transaction");
+  
+   /* get records from store and set to a variable */
+   const getAll = transactionObjectStore.getAll();
+
+   /* this function will trigger upon a successful .getAtt() execution */
+   getAll.onsuccess = function () {
+     if (getAll.result.length > 0) {
+       fetch("/api/transaction", {
+         method: "POST",
+         body: JSON.stringify(getAll.result),
+         headers: {
+           Accept: "application/json, text/plain, */*",
+           "Content-Type": "application/json",
+         },
+        })
+        
+
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open more transaction
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          // access the new transaction object store
+          const transactionObjectStore =
+            transaction.objectStore("new_transaction");
+          
+          transactionObjectStore.clear();
+
+          alert("All saved transactions have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadTransactions);
+
+    
